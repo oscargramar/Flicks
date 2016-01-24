0d435229ef922f8375adaebf4247fb2eb618b17f
Oscar G.M <oscar@wustl.edu>
2016-01-23 21:11:47 -0600

    3 parts done

---
+++ MovieViewer/MovieViewer/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  MovieViewer
+//
+//  Created by Oscar G.M on 1/23/16.
+//  Copyright © 2016 GMStudios. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
