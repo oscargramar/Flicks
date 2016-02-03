eb6b82841f3e6562536974c90f828c8b892cfe1e
Oscar G.M <oscar@wustl.edu>
2016-02-02 19:46:06 -0600

    scroll view for detailsVC done

--- MovieViewer/MovieViewer/DetailViewController.swift
+++ MovieViewer/MovieViewer/DetailViewController.swift
@@ -13,18 +13,25 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var posterImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var infoView: UIView!
     
     var movie: NSDictionary!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        scrollView.contentSize = CGSize(width: scrollView.frame.size.width, height: infoView.frame.size.height + infoView.frame.origin.y)
+        
+        
         
         let title = movie["title"] as? String
         titleLabel.text = title
         
         let overview = movie["overview"] as? String
+        
         overviewLabel.text = overview
+        overviewLabel.sizeToFit()
         
         if let posterPath = movie["poster_path"] as? String{
             let baseUrl = "http://image.tmdb.org/t/p/w500"
